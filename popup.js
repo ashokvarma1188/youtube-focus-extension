1d5ee3b5be5bcee4ebcbd055fbf7af34e855c15f
Ashok Varma <ashokvarma5247@gmail.com>
2026-06-23 20:16:14 +0530

    Add commented-out code for distraction hiding

--- popup.js
+++ popup.js
@@ -9,6 +9,7 @@
 //     "ytd-browse[page-subtype='home'] #contents"
 // ];
 
+
 // function hideDistractions() {
 //     chrome.storage.local.get("focusMode", (data) => {
 //         if (data.focusMode === false) {
